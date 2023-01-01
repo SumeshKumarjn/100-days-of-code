149b1755f01acdc7b9971ba0c17b9dd8222d64dc
SumeshKumarjn <117620920+SumeshKumarjn@users.noreply.github.com>
2023-01-01 08:09:30 +0530

    Add files via upload

---
+++ DAY 11/d11.js
@@ -0,0 +1,8 @@
+const element = document.getElementById('heading');
+console.log(element.innerHTML);
+
+const division = document.querySelector('heading');
+console.log(division.innerHTML);
+
+const innerTextElement = document.getElementById('innerid');
+console.log(innerTextElement);
---
+++ DAY 11/d11a.js
@@ -0,0 +1,9 @@
+const element = document.getElementById('heading');
+console.log(element.innerHTML);
+
+const division = document.querySelector('heading');
+console.log(division.innerHTML);
+
+const innerTextElement = document.getElementById('innerid');
+console.log(innerTextElement.innerText);
+console.log(innerTextElement.innerHTML);
