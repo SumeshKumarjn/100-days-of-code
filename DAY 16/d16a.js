b7d1bdbbd99e0df0b41681650d2560866853c397
SumeshKumarjn <117620920+SumeshKumarjn@users.noreply.github.com>
2023-01-08 14:24:32 +0530

    Add files via upload

---
+++ DAY 16/d16a.js
@@ -0,0 +1,7 @@
+const element = document.getElementById('username');
+element.placeholder = 'username';
+//element.value = 'sample test';
+//element.readOnly = false; // property
+//element.readOnly = true; // attribute
+element.setAttribute('readonly', true);
+console.log(element.getAttribute('value'));
