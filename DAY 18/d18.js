260b4c46d224fa21752fa359a38ea7e51c399d3f
SumeshKumarjn <117620920+SumeshKumarjn@users.noreply.github.com>
2023-01-10 12:15:04 +0530

    Add files via upload

---
+++ DAY 18/d18.js
@@ -0,0 +1,32 @@
+dynamicUI();
+function dynamicUI(){
+    const divElement = document.createElement('div');
+    divElement.setAttribute('id','divElementID');
+
+    const paraElement = document.createElement('p');
+    paraElement.innerHTML = 'Login';
+    paraElement.style.display = 'flex';
+    paraElement.style.justifyContent = 'center';
+
+    const inputElement = document.createElement('iput');
+    inputElement.placeholder = 'Enter username';
+    inputElement.id = 'Username_id';
+    inputElement.type = 'text';
+
+    const passwordElement = document.createElement('iput');
+    passwordElement.placeholder = 'Enter password';
+    passwordElement.id = 'password_id';
+    passwordElement.type = 'password';
+
+    const btnElement = document.createElement('button');
+    btnElement.placeholder = 'Login';
+    btnElement.id = 'btn_id';
+    btnElement.type = 'button';
+
+    divElement.appendChild(paraElement);
+    divElement.appendChild(inputElement);
+    divElement.appendChild(passwoedElement);
+    divElement.appendChild(btnElement);
+
+    document.body.appendChild(divElement);
+}
